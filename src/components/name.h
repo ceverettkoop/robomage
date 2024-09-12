328b0d9b4540ba7e5da74f14739cf40d20e4aec4
Sean <brennsto@gmail.com>
2024-09-12 09:15:08 -0700

    ECS from austinmoral.com

---
+++ src/components/name.h
@@ -0,0 +1,10 @@
+#ifndef NAME_H
+#define NAME_H
+
+#include <string>
+
+struct Name{
+    std::string value;
+};
+
+#endif /* NAME_H */
---
+++ src/ecs/entity.h
@@ -0,0 +1,13 @@
+#ifndef ENTITY_H
+#define ENTITY_H
+
+#include <cstdint>
+
+
+// credit https://austinmorlan.com/posts/entity_component_system/
+
+typedef uint32_t Entity;
+const uint32_t MAX_ENTITIES = 5000;
+
+
+#endif /* ENTITY_H */
