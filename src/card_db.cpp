b6c894c873a68c6bf9452bdf793049635e63ddd0
Sean McBride <brennsto@gmail.com>
2024-10-06 17:39:55 -0700

    parse

--- src/card_db.cpp
+++ src/card_db.cpp
@@ -1,18 +1,9 @@
 #include "card_db.h"
 
-const Card lightning_bolt = {
-    "Lightning Bolt", 
-    "Deals three damage to any target",
-    0
-};
+std::unordered_map<std::string, Card> card_db;
 
-const Card mountain = {
-    "Mountain", 
-    "{T} Add {R} to your mana pool",
-    1
-};
+void load_card(std::string card_name) {
 
-std::unordered_map<uint32_t, Card> card_db = {
-    {0, lightning_bolt},
-    {1, mountain}
-};
+
+
+}
--- src/card_db.h
+++ src/card_db.h
@@ -7,11 +7,14 @@
 
 //this is the underlying card, a token or copy would not have these traits
 struct Card{
+    std::string uid;
     std::string name;
     std::string oracle_text;
-    uint32_t uid;
 };
 
-extern std::unordered_map<uint32_t, Card> card_db;
+extern std::unordered_map<std::string, Card> card_db;
+
+//cards are loaded into db on demand
+void load_card(std::string card_name);
 
 #endif /* CARD_H */
